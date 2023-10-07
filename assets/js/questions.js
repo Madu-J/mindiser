9af3141104af6483bc675ae7a29d6c0e01886c20
Madu-J <>
2023-10-07 19:10:44 +0000

    Create an array for questions, Anwers and options

---
+++ assets/js/questions.js
@@ -0,0 +1,81 @@
+// Create an array for question answers and options
+let questions = [
+    {
+        num: 1,
+        question: "In which continent is Biafra located?",
+        answer: "Africa",
+        options: [
+            "Asia",
+            "Europe",
+            "Africa",
+            "America"
+        ]
+    },
+    {
+        num: 2,
+        question: "How many years does Biafria existed and when?",
+        answer: "3 years 1967 to 1970",
+        options: [
+            "5 years 1925 to 1930",
+            "4 years 1914 to 1918",
+            "10 years 1980 to 1990",
+            "3 years 1967 to 1970"
+        ]
+
+    },
+    {
+        num: 3,
+        question: "Biafra has many site attraction, which one of these is the most significant?",
+        answer: "Ogbunike-Cave",
+        options: [
+            "Ogbunike-Cave",
+            "Igboukwu museum",
+            "National war museum",
+            "Amusement park"
+        ]
+    },
+    {
+        num: 4,
+        question: "Who was the leader of Biafra?",
+        answer: "Odimegwu Ojukwu",
+        options: [
+            "Simon Ekpa",
+            "Nnamdi Kanu",
+            "Odimegwu Ojukwu",
+            "Ralph Uwazurike"
+        ]
+    },
+    {
+        num: 5,
+        question: "Which of these organsitions is currently piloting the affiars of the Biafra people?",
+        answer: "Biafra Republic Goverment in Exile",
+        options: [
+            "Indigenous People of Biafra",
+            "Biafra Republic Goverment in Exile",
+            "Federal Goverment of Nigeria",
+            "The Movement for the Actualization of the Sovereign State of Biafra"
+        ]
+    },
+    {
+        num: 6,
+        question: "Which of the these are Biafra speaking language?",
+        answer: "Igbo-Ibibio-Igala-Izon",
+        options: [
+            "Igbo, Hausa, Izon, Ebira",
+            "Yoruba, Igbo, Hausa",
+            "Ibibio, Jukun, Koro, Idoma",
+            "Igbo, Ibibio, Izon, Ebira"
+        ]
+    },
+    {
+        num: 7,
+        question: "What was the most weapon used against Biafra during the civil war?",
+        answer: "Air, Land, Sea blockade and starvation",
+        options: [
+            "Air, Land, Sea blockade and starvation",
+            "Economic blockade, seized of properties",
+            "Less soldiers, less weapons",
+            "No air force"
+        ]
+    }
+];
