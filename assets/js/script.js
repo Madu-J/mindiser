8a7604fb1399a6dc1c5f31bb1959cf81f5dafde3
Madu-J <>
2023-10-08 16:25:50 +0000

    Getting questions and options from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,32 @@ let exit_game_btn = document.querySelector(".buttons exit_game");
 
 // Start Quiz button active
 start_btn.onclick = () => {
-  info_box.classList.add("activeInfo");
+    info_box.classList.add("activeInfo");
 };
 
 // Exit Quiz button active
 exit_btn.onclick = () => {
-  info_box.classList.remove("activeInfo");
+    info_box.classList.remove("activeInfo");
 };
 
 // Contiune Quiz button active
 contiune_btn.onclick = () => {
-  info_box.classList.remove("activeInfo");
-  quiz_box.classList.add("activeQuiz");
+    info_box.classList.remove("activeInfo"); //Hide quiz info box
+    quiz_box.classList.add("activeQuiz"); //Show the quiz box
+    showQuestions(6);
 };
+
+let que_count = 0;
+
+//Questions and options 
+function showQuestions(index) {
+    let question_text = document.querySelector(".question_text");
+    let option_list = document.querySelector(".option_list");
+    let que_tag = '<span>' + questions[index].question + '</span>';
+    let option_tag = '<div class="option_list">' + questions[index].options[0] + '<span></span></div>';
+    + '<div class="option_list">' + questions[index].options[1] + '<span></span></div>';
+    + '<div class="option_list">' + questions[index].options[2] + '<span></span></div>';
+    + '<div class="option_list">' + questions[index].options[3] + '<span></span></div>';
+    question_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+}
