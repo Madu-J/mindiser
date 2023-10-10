b6075488a3ae29ac55f9e8911ca2aee542e4acde
Madu-J <>
2023-10-10 15:16:30 +0000

    Make user timer active

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let info_box = document.querySelector(".info_box");
 let exit_btn = info_box.querySelector(".buttons .exit");
 let contiune_btn = info_box.querySelector(".buttons .contiune");
 let quiz_box = document.querySelector(".quiz_box");
+let timeCount = quiz_box.querySelector(".timer .timer_sec");
 
 let option_list = document.querySelector(".option_list");
 
@@ -26,10 +27,13 @@ contiune_btn.onclick = () => {
     quiz_box.classList.add("activeQuiz"); //Show the quiz box
     showQuestions(0);
     queCounter(1);
+    startTimer(25);
 };
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let timeValue = 25;
 
 //Nest question button active
 let next_btn = quiz_box.querySelector(".next_btn");
@@ -40,6 +44,8 @@ next_btn.onclick = () => {
         que_numb++;
         showQuestions(que_count);
         queCounter(que_numb);
+        clearInterval(counter);
+        startTimer(timeValue);
     } else {
         console.log("Congratulations, you have completed the Quiz!");
     }
@@ -60,11 +66,12 @@ function showQuestions(index) {
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
 }
-
+//User answer icon
 let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 function optionSelected(answer) {
+    clearInterval(counter);
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
     let allOptions = option_list.children.length;
@@ -81,6 +88,7 @@ function optionSelected(answer) {
         for (let i = 0; i < allOptions; i++) {
             if (option_list.children[i].textContent == correctAns) {
                 option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
             }
         }
 
@@ -89,6 +97,14 @@ function optionSelected(answer) {
     //Disable other options once user select answer
     for (let i = 0; i < allOptions; i++) {
         option_list.children[i].classList.add("disabled");
+    }
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCount.textContent = time;
+        time--;
     }
 }
 
