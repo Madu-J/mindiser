56734e0f1470610df3cb3ece4dc312a5aeb8c40e
Madu-J <>
2023-10-10 11:53:00 +0000

    Make user answer active

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@
 let start_btn = document.querySelector(".start_btn button");
 let info_box = document.querySelector(".info_box");
 let exit_btn = info_box.querySelector(".buttons .exit");
-let contiune_btn = document.querySelector(".buttons .contiune");
+let contiune_btn = info_box.querySelector(".buttons .contiune");
 let quiz_box = document.querySelector(".quiz_box");
+
+let option_list = document.querySelector(".option_list");
+
 let replay_btn = document.querySelector(".buttons replay");
 let exit_game_btn = document.querySelector(".buttons exit_game");
 
@@ -44,31 +47,15 @@ next_btn.onclick = () => {
 
 //Questions and options
 function showQuestions(index) {
-    let question_text = document.querySelector(".question_text");
-    let option_list = document.querySelector(".option_list");
-    let que_tag =
-        "<span>" +
-        questions[index].numb +
-        "." +
-        questions[index].question +
-        "</span>";
-    let option_tag =
-        '<div class="option_list">' +
-        questions[index].options[0] +
-        "<span></span></div>";
-    +'<div class="option_list">' +
-        questions[index].options[1] +
-        "<span></span></div>";
-    +'<div class="option_list">' +
-        questions[index].options[2] +
-        "<span></span></div>";
-    +'<div class="option_list">' +
-        questions[index].options[3] +
-        "<span></span></div>";
-    question_text.innerHTML = que_tag;
+    let que_text = document.querySelector(".question_text");
+    let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
+    let option_tag = '<div class="option">' + questions[index].options[0] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+    que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
-
-    let option = option_list.querySelectorAll(".option_list");
+    let option = option_list.querySelectorAll(".option");
     for (let i = 0; i < option.length; i++) {
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
@@ -77,6 +64,7 @@ function showQuestions(index) {
 function optionSelected(answer) {
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
     if (userAns == correctAns) {
         answer.classList.add("correct");
         console.log("You are correct");
@@ -84,15 +72,18 @@ function optionSelected(answer) {
         answer.classList.add("incorrect");
         console.log("Incorrect answer");
     }
+
+    for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
 }
+
+
+
+
 
 function queCounter(index) {
     let bottom_ques_counter = quiz_box.querySelector(".total-question");
-    let totalQuesCountTag =
-        "<span><p>" +
-        index +
-        "</p>of<p>" +
-        questions.length +
-        "</p>Questions</span>";
+    let totalQuesCountTag = '<span><p>' + que_count + '</p>of<p>' + questions.length + '</p>Questions</span>';
     bottom_ques_counter.innerHTML = totalQuesCountTag;
 }
